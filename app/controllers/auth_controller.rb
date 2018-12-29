4ad5e1ce2a20cbaa30464c01e3bba942c1f30e26
github username <github email address>
2018-12-29 12:32:09 -0500

    login avail but not persisting

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,10 +4,19 @@ class AuthController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      render json: {message: "correct username & password"}, status: :accepted
+      render json: {
+      message: "correct username & password",
+      user_info: @user,
+      error: false
+    },
+      status: :accepted
       # redirect to: :profile
     else
-      render json: {message: "incorrect! are you a h3cker?!"}, status: :unauthorized
+      render json: {
+      message: "incorrect! are you a h3cker?!",
+      error: true
+    },
+      status: :unauthorized
       # redirect to: :home
     end
   end
