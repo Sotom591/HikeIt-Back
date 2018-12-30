d95d8a7566f7c3c3d3da9411bc0b90776d54c9c7
github username <github email address>
2018-12-29 19:13:28 -0500

    login persists, complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
+require 'byebug'
 class UsersController < ApplicationController
   def index
       render json: User.all
+  end
+
+  def profile
+    token = request.headers["Authentication"].split(" ")[1]
+    payload = decode(token)
+    user_id = payload["user_id"]
+    render json: {user: User.find(user_id) }, status: :accepted
   end
 
 private
